4fc40219bfc0688f4db67f873c19b56ac293f0f4
liuzhao2580 <1457639270@qq.com>
2019-12-15 20:28:15 +0800

    继续初步

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
     // 入口文件的配置
     entry: "./src/index.js",
@@ -15,5 +16,22 @@ module.exports = {
     // 用于设置文件的导出方式 主要是判断是否压缩代码
 	// development 开发环境
 	// production  项目部署的生产环境  默认为生产环境
-    mode: "production"
+    mode: "production",
+    // webpack-dev-server 的基本配置
+    devServer: {
+        open: true, // 自动打开浏览器
+        port: 9527, // 设置运行的端口
+        hot: true, // 设置热编译 简单来说就是每次编译的时候, 会编译改变的文件
+        compress: true, // 打包的文件进行压缩
+        contentBase: "./public" // 配置默认的入口文件  默认的是 / 项目根目录
+    },
+    plugins: [
+        // // 配置 html-webpack-plugin 
+        new HtmlWebpackPlugin({
+            // 设置打包的文件名称
+            filename: "index.html",
+            // 设置需要打包的文件路径
+            template: "./public/index.html"
+        })
+    ]
 }

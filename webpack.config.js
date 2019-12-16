4394de2d96579d51bcbb2ca6c071fe3facec0421
小火车况且况且况且 <1457639270@qq.com>
2019-12-16 17:16:15 +0800

    file-loader url-loader 使用

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,34 @@ module.exports = {
             {
                 test: /\.s(a|c)ss$/,
                 use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            // 用来配置 file-loader 和 url-loader
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        /* 
+                        用来限制图片文件的大小
+                        如果图片大于了 5KB的时候, 就以路径的形式展示
+                        小于的话就是用base64格式展示
+                        */
+                        limit: 5 * 1024,
+                        /* 
+                        设置打包输出的目录
+                        这里就是在 dist 文件下新建一个 status/images
+                        用于存放图片
+                        */
+                        outputPath: "status/images",
+                        /* 
+                        设置打包的图片的名称
+                        [name]: 表示当前的文件使用原始名称
+                        [hash:8]: 表示文件使用 8位hash格式的命名
+                        [ext]: 表示文件的后缀名称保持原样
+                        */
+                        name: '[name]-[hash:8].[ext]'
+                    }
+                }]
             }
         ]
     }
